682b5e92af35bef8f9a5c27ebe9aecb76d354485
ozcanmelih <melih.ozcan89@gmail.com>
2020-11-17 23:14:53 +0300

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(userId => dispatch(fetchUser(userId)));
+};
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response.data
+    });
+};
+
+export const fetchUser = userId => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${userId}`);
+
+    dispatch({
+        type: 'FETCH_USER',
+        payload: response.data
+    });
+};
+
+// const _fetchUser = _.memoize(async (userId, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${userId}`);
+
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     });
+// });
